1871100ad2478a3a554cea4e18cb60aab1179796
Buggxp <thinkorganicafrica@gmail.com>
2025-04-05 22:37:19 +0300

    Add ReentrancyGuard.sol and update Escrow.sol, hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20", // Add this version
+      },
+      {
+        version: "0.8.19", // Keep this if you have contracts requiring 0.8.19
+      },
+    ],
+  },
 };
