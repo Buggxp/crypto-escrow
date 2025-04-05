ff7d1a2516a7f8cd70bd43f1b90378ecc47039ae
Buggxp <thinkorganicafrica@gmail.com>
2025-04-05 23:27:37 +0300

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,10 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20", // Add this version
+        version: "0.8.20",
       },
       {
-        version: "0.8.19", // Keep this if you have contracts requiring 0.8.19
+        version: "0.8.19",
       },
     ],
   },
